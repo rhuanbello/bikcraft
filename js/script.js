d58af241c262e9e5118dbef93a1c19b24fa7ad56
rhuanbello <rhuambello@gmail.com>
2021-06-14 20:37:49 -0300

    Animações JavaScript

---
+++ js/script.js
@@ -0,0 +1,18 @@
+if(window.SimpleSlide) {
+    new SimpleSlide({
+        slide: "quote",
+        time: 5000
+    });
+
+    new SimpleSlide({
+        slide: "portfolio",
+        time: 5000,
+        nav: true 
+    });
+}
+
+if(window.SimpleAnime) {
+    new SimpleAnime({
+        
+    })
+}
